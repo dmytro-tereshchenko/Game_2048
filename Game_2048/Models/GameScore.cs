08bce51302c10149c1b294e3a1ead26fad4adfd0
Dmytro Tereshchenko <legat4life@gmail.com>
2021-04-02 23:19:04 +0300

    Add project files.

---
+++ Game_2048/Models/GameScore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_2048.Models
+{
+    internal sealed class GameScore
+    {
+        private int highScore;
+        private int score;
+
+        public GameScore() : this(0) { }
+
+        public GameScore(int highScore)
+        {
+            this.highScore = highScore;
+            this.score = 0;
+        }
+
+        public int Score
+        {
+            get => score;
+            set
+            {
+                if (!score.Equals(value) && value >= 0)
+                {
+                    score = value;
+                    OnScoreChanged(EventArgs.Empty);
+                }
+            }
+        }
+        public int HighScore
+        {
+            get => highScore;
+            set
+            {
+                if (!highScore.Equals(value) && value >= 0)
+                {
+                    highScore = value;
+                    OnHighScoreChanged(EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler<EventArgs> ScoreChanged;
+        public event EventHandler<EventArgs> HighScoreChanged;
+
+        private void OnScoreChanged(EventArgs e)
+        {
+            ScoreChanged?.Invoke(this, e);
+        }
+        private void OnHighScoreChanged(EventArgs e)
+        {
+            HighScoreChanged?.Invoke(this, e);
+        }
+    }
+}
---
+++ Game_2048/Models/HighScoreFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_2048.Models
+{
+    internal static class HighScoreFile
+    {
+        public const string Path = @"..\..\HighScoreFile.xml";
+
+        public static class Root
+        {
+            public const string ElementName = "Scores";
+
+            public static class Score
+            {
+                public const string ElementName = "Score";
+
+                public static class Attributes
+                {
+                    public const string HighScore = "HighScore";
+                }
+            }
+        }
+    }
+}
